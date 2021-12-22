c55ec4585d562a7363bdc0bbaaa7ae27f26b6b3f
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2021-12-22 11:06:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+const menuicon=document.querySelector('.hamburger_menu');
+const mobile_navbar=document.querySelector('.mobile_navbar')
+ menuicon.addEventListener("click", () => {
+     menuicon.classList.toggle("change");
+     let state=window.getComputedStyle(mobile_navbar);
+     if (state.display == "none"){
+        mobile_navbar.style.display="block";
+     }
+     else if (state.display == "block"){
+        mobile_navbar.style.display="none";
+ 
+     }
+     
+ })
+
+
